e2f821d420704212f91c83bc56eae9d3191301cf
saurabhjaykar1603 <jaykarsaurabh97@gmail.com>
2023-09-26 12:33:51 +0530

    feat : navbar added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,54 @@
-import React from 'react'
-import  './App.css'
+import React from "react";
+import "./App.css";
 
 const App = () => {
   return (
     <>
+      <nav className="navbar navbar-expand-lg   py-3" style={{backgroundColor: "#00D6F4"}}>
+        <div className="container-fluid">
+          <a className="navbar-brand fs-3 fw-600 " href="/">
+           Data Filter X
+          </a>
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul className="navbar-nav me-auto mb-2 mb-lg-0 fs-5 fw-bold">
+              <li className="nav-item">
+                <a className="nav-link active" aria-current="page" href="/">
+                  Home
+                </a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="/">
+                 About
+                </a>
+              </li>
+            </ul>
+            <form className="d-flex" role="search">
+              <input
+                className="form-control me-2"
+                type="search"
+                placeholder="Search"
+                aria-label="Search"
+              />
+              <button className="btn btn-outline-success" type="submit">
+                Search
+              </button>
+            </form>
+          </div>
+        </div>
+      </nav>
     </>
-  )
-}
+  );
+};
 
-export default App
+export default App;
